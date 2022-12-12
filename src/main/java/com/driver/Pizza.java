172fdce141454663fb80952fe268243de22392bc
VN Reddy Naini <vnreddynaini@VNs-MacBook-Air.local>
2022-12-13 01:35:04 +0530

    pizza billing 1

--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -6,29 +6,50 @@ public class Pizza {
     private Boolean isVeg;
     private String bill;
 
+    private int res;
+
     public Pizza(Boolean isVeg){
         this.isVeg = isVeg;
         // your code goes here
     }
 
     public int getPrice(){
-        return this.price;
+        if (isVeg) {
+            price = 300;
+            return this.price;
+        }else {
+            price = 400;
+            return this.price;
+        }
     }
 
     public void addExtraCheese(){
         // your code goes here
+        res = res + 80;
+        System.out.println("Extra Cheese Added: 80");
     }
 
     public void addExtraToppings(){
         // your code goes here
+        if(isVeg){
+            res = res + 70;
+            System.out.println("Extra Toppings Added: 70");
+        }else{
+            res = res + 120;
+            System.out.println("Extra Toppings Added: 120");
+        }
     }
 
     public void addTakeaway(){
         // your code goes here
+        res = res + 20;
+        System.out.println("Paperbag Added: 20");
     }
 
     public String getBill(){
         // your code goes here
+        bill = bill+getPrice();
+        bill = Integer.toString(res);
         return this.bill;
     }
 }
